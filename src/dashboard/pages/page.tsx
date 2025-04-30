d91037aff39ed3b3089900886f245367e34d1abe
Nícholas Oliveira <nicholas.andre@10up.com>
2025-04-30 15:09:37 -0300

    initial pass at wix auth

--- src/dashboard/pages/page.tsx
+++ src/dashboard/pages/page.tsx
@@ -1,14 +1,27 @@
-import React, { type FC, useState } from 'react';
+import React, { type FC, useEffect, useState } from 'react';
 import { dashboard } from '@wix/dashboard';
 import { Button, Page, WixDesignSystemProvider, Box, Text, Layout, Cell } from '@wix/design-system';
 import '@wix/design-system/styles.global.css';
 import * as Icons from '@wix/wix-ui-icons-common';
-import { getAppInstance, getClientUrl } from '../../utils/wix';
+import { httpClient } from '@wix/essentials';
+import { getAppInstance } from '../../utils/wix';
 
 const Index: FC = () => {
 	const [isConnected, setIsConnected] = useState(false);
+	const [authUrl, setAuthUrl] = useState('');
+
+	useEffect(() => {
+		const getAuthUrl = async () => {
+			const url = await getAuthorizationUrl();
+			setAuthUrl(url);
+		};
+		getAuthUrl();
+	}, []);
 
 	const handleConnectJobber = async () => {
+		console.log('handleConnectJobber', import.meta.env.BASE_API_URL);
+		const res = await httpClient.fetchWithAuth(`${import.meta.env.BASE_API_URL}/token`);
+		console.log(await res.text());
 		// TODO: Implement Jobber connection logic
 		setIsConnected(true);
 		dashboard.showToast({
@@ -16,10 +29,13 @@ const Index: FC = () => {
 		});
 	};
 
-	const getAuthUrl = () => {
+	const getAuthorizationUrl = async () => {
 		const token = getAppInstance();
-		const clientUrl = window.location.origin;
-		const returnUrl = window.location.href;
+		const clientUrl = import.meta.env.BASE_API_URL;
+		const returnUrl = await dashboard.getPageUrl({
+			pageId: '2b8b12d1-88a3-41e1-bd8e-c67c6a774b7b',
+			relativeUrl: '?referal=page-3',
+		});
 		return `http://localhost:8000/wix/auth?jobber_token=${token}&clientUrl=${clientUrl}&returnUrl=${returnUrl}`;
 	};
 
@@ -31,8 +47,7 @@ const Index: FC = () => {
 					subtitle="Connect your Jobber account to your Wix site."
 					actionsBar={
 						<Button
-							as="a"
-							href={getAuthUrl()}
+							onClick={handleConnectJobber}
 							prefixIcon={<Icons.Link />}
 							priority="primary"
 							disabled={isConnected}
@@ -65,9 +80,11 @@ const Index: FC = () => {
 								{!isConnected && (
 									<Button
 										as="a"
-										href={getAuthUrl()}
+										target="_blank"
+										href={authUrl}
 										prefixIcon={<Icons.Link />}
 										priority="primary"
+										disabled={!authUrl}
 									>
 										Connect Jobber
 									</Button>
