74c13214808fbd39bcfd5a61e08d229db9ae33e3
Nícholas André <nicholas.andre@10up.com>
2025-05-16 16:55:42 -0300

    app removal wix event

---
+++ src/backend/events/app-removal/event.ts
@@ -0,0 +1,19 @@
+import { appInstances } from '@wix/app-management';
+import { auth } from '@wix/essentials';
+import { getMiddlewareUrl } from '../../../utils/api';
+
+appInstances.onAppInstanceRemoved(async () => {
+	try {
+		console.log('appInstances.onAppInstanceRemoved');
+		const elevatedFetch = auth.elevate(fetch);
+		await elevatedFetch(`${getMiddlewareUrl()}/disconnect`, {
+			method: 'POST',
+			headers: {
+				'x-jobber-integration': 'wix',
+			},
+		});
+		console.log('appInstances.onAppInstanceRemoved success');
+	} catch (error) {
+		console.error('Failed to disconnect during app removal:', error);
+	}
+});
