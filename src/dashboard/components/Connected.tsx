465588d9cc4c033a88cd9612dffedeee7d2da67c
Nícholas Oliveira <nicholas.andre@10up.com>
2025-05-19 15:37:57 -0300

    wip

--- src/dashboard/components/Connected.tsx
+++ src/dashboard/components/Connected.tsx
@@ -1,17 +1,45 @@
 import React, { type FC } from 'react';
-import { Box, Text } from '@wix/design-system';
-import { Check as CheckIcon } from '@wix/wix-ui-icons-common';
+import { Box, Text, EmptyState, Button } from '@wix/design-system';
+import { Check as CheckIcon, Edit as EditIcon } from '@wix/wix-ui-icons-common';
 
 export const Connected: FC = () => (
-	<>
-		<Box align="center" gap="12px" verticalAlign="middle">
-			<CheckIcon size="48px" color="#389E0D" />
-			<Text size="medium" weight="normal">
-				Your Jobber account is successfully connected!
-			</Text>
+	<EmptyState
+		title="Successfully Connected to Jobber"
+		subtitle="Your Jobber account is now connected to your Wix site."
+		theme="page-no-border"
+		image={<CheckIcon size="48px" color="#389E0D" />}
+	>
+		<Box direction="vertical" gap="24px" align="center">
+			<Box direction="vertical" gap="12px" align="center">
+				<Text size="medium" weight="normal">
+					You can now embed Jobber forms on your site
+				</Text>
+				<Text size="small" secondary>
+					To add Jobber forms to your site:
+				</Text>
+				<Box direction="vertical" gap="8px" align="center">
+					<Text size="small" secondary>
+						1. Go to your site editor
+					</Text>
+					<Text size="small" secondary>
+						2. Add the Jobber Forms widget to your page
+					</Text>
+					<Text size="small" secondary>
+						3. Configure the form settings in the widget
+					</Text>
+				</Box>
+			</Box>
+			<Button
+				prefixIcon={<EditIcon />}
+				priority="secondary"
+				onClick={() => {
+					if (window.top) {
+						window.top.location.href = '/editor';
+					}
+				}}
+			>
+				Open Site Editor
+			</Button>
 		</Box>
-		<Text size="small" secondary>
-			You can now embed Jobber forms directly from your Wix site.
-		</Text>
-	</>
+	</EmptyState>
 );
