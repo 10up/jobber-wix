2e9940c773cd6b37615290aee1e25c84f674140f
Nícholas Oliveira <nicholas.andre@10up.com>
2025-05-19 09:52:56 -0300

    console.log evend data

--- src/backend/events/app-removal/event.ts
+++ src/backend/events/app-removal/event.ts
@@ -2,7 +2,8 @@ import { appInstances } from '@wix/app-management';
 import { auth } from '@wix/essentials';
 import { getMiddlewareUrl } from '../../../utils/api';
 
-appInstances.onAppInstanceRemoved(async () => {
+appInstances.onAppInstanceRemoved(async (data) => {
+	console.log(data);
 	try {
 		console.log('appInstances.onAppInstanceRemoved');
 		const elevatedFetch = auth.elevate(fetch);
