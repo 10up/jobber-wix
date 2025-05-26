db3aa69332b0f07d2776c863a6c6b5f7efc9e53b
Nícholas Oliveira <nicholas.andre@10up.com>
2025-05-26 13:56:42 -0300

    add logging

--- src/backend/events/app-removal/event.ts
+++ src/backend/events/app-removal/event.ts
@@ -4,12 +4,14 @@ import { getMiddlewareUrl } from '../../../utils/api';
 
 appInstances.onAppInstanceRemoved(async () => {
 	try {
-		httpClient.fetchWithAuth(`${getMiddlewareUrl()}/disconnect`, {
+		console.log('App Removal Event: Disconnecting from Jobber');
+		const res = await httpClient.fetchWithAuth(`${getMiddlewareUrl()}/disconnect`, {
 			method: 'POST',
 			headers: {
 				'x-jobber-integration': 'wix',
 			},
 		});
+		console.log('App Removal Event: Disconnected from Jobber', await res.text());
 	} catch (error) {
 		console.error('Failed to disconnect during app removal:', error);
 	}
